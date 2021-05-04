cc08c15c3681f3a1b684b4ac0084d1676465d2a5
cuongnguyen71120 <cuongnv.ba9011@st.usth.edu.vn>
2021-05-04 10:18:03 +0700

    Submit pratica work 3

--- 3.pratical.work.server.setup.c
+++ 3.pratical.work.server.setup.c
@@ -7,13 +7,13 @@
 # define PORT 8784;
 
 int main(){
-    struct sockaddr_in severaddr,caddr;
-    int sever_family,bind_create,listen_port;
+    struct sockaddr_in serveraddr,caddr;
+    int server_family,bind_create,listen_port;
     int clen,accept_create;
     unsigned short port = 8784;
     //create socket
-    sever_family=socket(AF_INET,SOCK_STREAM,0);
-    if (sever_family<0){
+    server_family=socket(AF_INET,SOCK_STREAM,0);
+    if (server_family<0){
         printf("Could not create socket.....\n");
         exit(0);
     }
@@ -21,14 +21,14 @@ int main(){
     {
         printf("Socket created\n");
     }
-    memset(&severaddr,0,sizeof(severaddr));
-    severaddr.sin_family=AF_INET;
-    severaddr.sin_addr.s_addr=AF_INET;
-    severaddr.sin_port=htons(port);
-    inet_pton(AF_INET,"0.0.0.0",&severaddr.sin_addr);
+    memset(&serveraddr,0,sizeof(serveraddr));
+    serveraddr.sin_family=AF_INET;
+    serveraddr.sin_addr.s_addr=AF_INET;
+    serveraddr.sin_port=htons(port);
+    inet_pton(AF_INET,"0.0.0.0",&serveraddr.sin_addr); // "0.0.0.0" is the IPv4 address and using inet_pton to convert the IP address from text format to numeric binary 
 
     //create the bind
-    bind_create=bind(sever_family,(struct sockaddr*)&severaddr,sizeof(severaddr));
+    bind_create=bind(server_family,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
     if(bind_create<0){
         printf("Error binding\n");
         exit(0);
@@ -36,7 +36,7 @@ int main(){
     printf("Bind done\n");
 
     //create the listen 
-    listen_port=listen(sever_family,5);
+    listen_port=listen(server_family,5);
     if(listen_port<0){
         printf("Error listening\n");
         exit(0);
@@ -47,12 +47,11 @@ int main(){
 
     //create the accept function 
     clen=sizeof(caddr);
-    accept_create=accept(sever_family,(struct sockaddr*)&caddr,&clen);
+    accept_create=accept(server_family,(struct sockaddr*)&caddr,&clen);
     if(accept_create<0){
         printf("Error accept\n");
         exit(0);
     }
-    else
     printf("Connection accepted");
     return 0;
 }
